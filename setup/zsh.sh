6d623100138c939281497900e20b17734fff4a50
Michele Loria <info@micheleloria.net>
2017-10-03 00:45:49 +0200

    Add oh-my-zsh custom files & fix setup script

--- setup/zsh.sh
+++ setup/zsh.sh
@@ -3,6 +3,8 @@ echo "Installing zsh configuration files, please wait ..."
 
 cd ~ && ln -s .dotfiles/zsh/zshrc .zshrc
 cd ~ && ln -s .dotfiles/zsh/oh-my-zsh .oh-my-zsh
+cd ~ && ln -s .dotfiles/zsh/oh-my-zsh-custom .oh-my-zsh/custom
+
 chmod 777 .dotfiles/vim/tmp
 
 echo "😄  done"
