c44ce23b5e7ce68702f18b34412e5516a261aaab
Michele Loria <info@micheleloria.net>
2017-10-03 16:28:59 +0200

    Fix zsh installer script

--- setup/zsh.sh
+++ setup/zsh.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-echo "Installing zsh configuration files, please wait ..."
-
-cd ~ && ln -s .dotfiles/zsh/zshrc .zshrc
-cd ~ && ln -s .dotfiles/zsh/bash_aliases .bash_aliases
+echo "Installing zsh configuration files, please wait ...\n"
+echo "*** NOTE: after installing oh-my-zsh, a new shell will be opened, pausing"
+echo "the script execution. Exit from that shell, with <C-D> to continue installing"
 
 cd ~ && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+rm ~/.zshrc
+cd ~ && ln -s .dotfiles/zsh/zshrc .zshrc
+cd ~ && ln -s .dotfiles/zsh/bash_aliases .bash_aliases
 
 echo "😄  done"
