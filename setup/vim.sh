238f21297ca41aab21d899c69d748a0120abfd46
Michele Loria <info@micheleloria.net>
2017-10-03 17:57:00 +0200

    Fix language warnings & update Vundle version

--- setup/vim.sh
+++ setup/vim.sh
@@ -6,5 +6,7 @@ cd ~ && ln -s .dotfiles/vim/vimrc .vimrc
 chmod 777 .dotfiles/vim/tmp
 
 echo "Installing Bundles ..."
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 vim +BundleInstall +qall
 echo "😄  done"
