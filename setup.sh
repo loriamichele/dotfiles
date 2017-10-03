5284e568295b3f91817859a33d82b8ddeb808d69
Michele Loria <info@micheleloria.net>
2017-10-03 16:02:54 +0200

    Fix typo

--- setup.sh
+++ setup.sh
@@ -12,9 +12,9 @@ echo "
 Instalation in progress ...
 "
 
-bash .setup/zsh.sh
-bash .setup/vim.sh
-bash .setup/font.sh
+bash setup/zsh.sh
+bash setup/vim.sh
+bash setup/font.sh
 
 echo "If no error happens, it means that everything has been installed."
 echo ""
