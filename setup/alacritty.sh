f3c0dd2973f8bf4b867ca5b07fd57b637b146005
Michele Loria <info@micheleloria.net>
2017-10-09 05:15:48 +0200

    Fix typo in alacritty install

--- setup/alacritty.sh
+++ setup/alacritty.sh
@@ -7,9 +7,9 @@ cd ~ && ln -s .dotfiles/alacritty/alacritty.yml .config/alacritty/alacritty.yml
 echo "Cloning and compiling Alacritty..."
 
 cd ~ && mkdir git_projects
-git clone https://github.com/jwilm/alacritty.git ~/git_projects
+git clone https://github.com/jwilm/alacritty.git ~/git_projects/alacritty
 curl https://sh.rustup.rs -sSf | sh
-cd ~/alacritty
+cd ~/git_projects/alacritty
 rustup override set stable
 rustup update stable
 cargo build --release
