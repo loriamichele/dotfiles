6fc7d002e24bbf57be04cb410b0d2856bff201e9
Michele Loria <info@micheleloria.net>
2019-05-28 13:55:37 +0200

    Some minor fixes/changes mostly for Linux hosts

--- setup/font.sh
+++ setup/font.sh
@@ -5,13 +5,13 @@ set -e
 echo "Installing fonts..."
 
 if [ ! -d "~/.local" ]; then
-    mkdir ~/.local
+    mkdir ~/.local || true
 fi
 if [ ! -d "~/.local/share" ]; then
-    mkdir ~/.local/share
+    mkdir ~/.local/share || true
 fi
 if [ ! -d "~/.local/share/fonts" ]; then
-    mkdir ~/.local/share/fonts
+    mkdir ~/.local/share/fonts || true
 fi
 
 cp font/* ~/.local/share/fonts
